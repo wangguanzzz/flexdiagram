5c1c4c5d7d03e71151141d7007981a1f8d87b280
chriswang <chriswang@localhost.localdomain>
2019-01-21 16:20:55 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Button, Jumbotron } from "react-bootstrap";
+import {
+  Button,
+  Jumbotron,
+  Grid,
+  Row,
+  Col,
+  FormGroup,
+  FormControl,
+  ControlLabel
+} from "react-bootstrap";
+import SequenceDiagram from "react-sequence-diagram";
+
+const input =
+  "Andrew->China: Says Hello\n" +
+  "Note right of China: China thinks\\nabout it\n" +
+  "China-->Andrew: How are you?\n" +
+  "Andrew->>China: I am good thanks!";
+
+const options = {
+  theme: "simple"
+};
+
+function onError(error) {
+  console.log(error);
+}
 
 class App extends Component {
   render() {
@@ -14,6 +38,26 @@ class App extends Component {
             display the system architect diagram
           </p>
         </Jumbotron>
+        <Grid>
+          <Row className="show-grid">
+            <Col md={6}>
+              <form>
+                <FormGroup>
+                  <ControlLabel>Diagram DSL</ControlLabel>
+                  <FormControl componentClass="textarea" rows="5" />
+                </FormGroup>
+              </form>
+            </Col>
+            <Col md={6}>
+              <SequenceDiagram
+                col-md-6
+                input={input}
+                options={options}
+                onError={onError}
+              />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
