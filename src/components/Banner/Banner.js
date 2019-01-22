dbe22ed69874680a3c981592256a3847807cf4ed
chriswang <chriswang@localhost.localdomain>
2019-01-22 12:05:34 +0800

    update

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Jumbotron } from "react-bootstrap";
+import { Jumbotron } from "reactstrap";
 
 const banner = () => {
   return (
--- src/components/Pannel/Pannel.js
+++ src/components/Pannel/Pannel.js
@@ -1,32 +1,22 @@
 import React from "react";
-import {
-  FormGroup,
-  FormControl,
-  ControlLabel,
-  Button,
-  InputGroup
-} from "react-bootstrap";
+import { FormGroup, Label, Button, InputGroup, Input } from "reactstrap";
 
 const pannel = props => {
   return (
     <form>
       <FormGroup>
-        <ControlLabel>Diagram DSL</ControlLabel>
-
-        <FormControl
-          componentClass="textarea"
+        <Label>Diagram DSL</Label>
+        <Input
+          type="textarea"
           value={props.dsl}
           rows="5"
           onChange={props.changed}
         />
       </FormGroup>
       <FormGroup>
-        <InputGroup>
-          <FormControl type="text" placeholder="DSL Name" />
-          <InputGroup.Button>
-            <Button bsStyle="primary">Save</Button>
-          </InputGroup.Button>
-        </InputGroup>
+        <Button block color="primary">
+          Save
+        </Button>
       </FormGroup>
     </form>
   );
